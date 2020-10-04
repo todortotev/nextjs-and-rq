5ccc52295450de0e84f1d7c30ce4c8ebb32c892f
TodorTotev <51530311+TodorTotev@users.noreply.github.com>
2020-10-04 14:54:30 +0300

    init

---
+++ hooks/usePosts/index.js
@@ -0,0 +1,18 @@
+import ky from 'ky-universal'
+import { useQuery } from 'react-query'
+
+const fetchPosts = async (_, limit) => {
+  const offset = limit ?? 10
+  const parsed = await ky('https://jsonplaceholder.typicode.com/posts').json()
+  const result = parsed.filter(x => x.id <= offset)
+  return result
+}
+
+const usePosts = (limit, isClient) => {
+  const query = useQuery(['posts', limit], fetchPosts, {
+    enabled: isClient,
+  })
+  return { ...query }
+}
+
+export { usePosts, fetchPosts }
